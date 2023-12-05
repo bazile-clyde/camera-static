90e1ce7bc5fc96e0007e1d92b69643fd590aef9d
Clyde Bazile <clyde@viam.com>
2023-12-05 10:57:52 -0500

    update

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"context"
+	"fmt"
+	vidio "github.com/AlexEidt/Vidio"
 	"github.com/edaniels/golog"
 	"github.com/pkg/errors"
-	"github.com/viamrobotics/gostream"
 	"go.viam.com/rdk/components/camera"
+	"go.viam.com/rdk/gostream"
+	"go.viam.com/rdk/gostream/codec/x264"
+	gimage "go.viam.com/rdk/gostream/image"
+	"go.viam.com/rdk/logging"
 	"go.viam.com/rdk/module"
 	"go.viam.com/rdk/resource"
 	"image"
@@ -21,7 +26,7 @@ func init() {
 
 func main() {
 	ctx := context.Background()
-	logger := golog.NewLogger("static_camera")
+	logger := logging.NewLogger("static_camera")
 
 	mod, err := module.NewModuleFromArgs(ctx, logger)
 	if err != nil {
@@ -49,9 +54,32 @@ func (s static) Close(_ context.Context) error {
 	return nil
 }
 
-func newCamera(ctx context.Context, _ resource.Dependencies, _ resource.Config, _ golog.Logger) (camera.Camera, error) {
+func handleErr(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		panic(err.Error())
+	}
+}
+
+func newCamera(ctx context.Context, _ resource.Dependencies, _ resource.Config, logger logging.Logger) (camera.Camera, error) {
+	// ffmpeg -f lavfi -i testsrc=duration=10:size=640x480:rate=30 testsrc.mp4
+	v, err := vidio.NewVideo("/usr/local/libs/camera-static/testsrc.mp4")
+	handleErr(err)
+
+	n := v.Frames()
+	i := 0
+	l := golog.NewLogger("newCamera")
+	w, h := 640, 480
+	encoder, _ := x264.NewEncoder(w, h, 30, l)
+
 	reader := gostream.VideoReaderFunc(func(ctx context.Context) (image.Image, func(), error) {
-		return image.NewYCbCr(image.Rect(0, 0, 640, 480), image.YCbCrSubsampleRatio420), func() {}, nil
+		defer func() { i = (i + 1) % n }()
+		img, err := v.ReadFrames(i)
+		handleErr(err)
+
+		bytes, err := encoder.Encode(ctx, img[0])
+		handleErr(err)
+		return gimage.NewH264Image(bytes), func() {}, err
 	})
 
 	cam := static{
@@ -64,5 +92,5 @@ func newCamera(ctx context.Context, _ resource.Dependencies, _ resource.Config, 
 	}
 
 	name := resource.NewName(camera.API, "static")
-	return camera.FromVideoSource(name, src), nil
+	return camera.FromVideoSource(name, src, logger), nil
 }
